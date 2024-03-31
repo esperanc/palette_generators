8821c0b86b1089eccb0a7d775fd6abca66c513a2
Claudio Esperança <claudio.esperanca@gmail.com>
2024-03-31 13:41:48 -0300

    Fixed interpolation

--- docs/components/shaderInterpolator.js
+++ docs/components/shaderInterpolator.js
@@ -47,17 +47,21 @@ function gradientMaker(options = {}) {
         precision mediump float;
         uniform float lrgb;
         varying vec4 color;
-        float fn (float c) {
+        float fnToRgb (float c) {
           float tmp = abs(c);
             if (tmp > 0.0031308) {
                 return 1.055 * pow(tmp, 1. / 2.4) - 0.055;
           }
             return c * 12.92;
         }
+        vec4 toRgb(vec4 color) {
+          return vec4 (fnToRgb(color.r), fnToRgb(color.g), fnToRgb(color.b), color.a);
+        }
         void main () {
-          gl_FragColor = (lrgb != 0.) ? vec4(fn(color.r),fn(color.g),fn(color.b),color.a) : color;
+          gl_FragColor = (lrgb != 0.) ? toRgb(color) : color;
         }`,
       vert: `
+        precision mediump float;
         attribute vec2 index;
         uniform vec4 color1;
         uniform vec2 position1;
@@ -65,17 +69,28 @@ function gradientMaker(options = {}) {
         uniform vec2 position2;
         uniform vec4 color3;
         uniform vec2 position3;
+        uniform float lrgb;
         varying vec4 color;
+        float fnToLrgb (float c) {
+          float tmp = abs(c);
+          if (tmp <= 0.04045) {
+                return c / 12.92;
+            }
+            return pow((tmp + 0.055) / 1.055, 2.4);
+        }
+        vec4 toLrgb(vec4 color) {
+          return vec4 (fnToLrgb(color.r), fnToLrgb(color.g), fnToLrgb(color.b), color.a);
+        }
         void main () {
           if (index.x == 1.) {
             gl_Position = vec4(position1,0,1);
-            color = color1;
+            color = (lrgb != 0.) ? toLrgb(color1) : color1;
           } else if (index.x == 2.) {
             gl_Position = vec4(position2,0,1);
-            color = color2;        
+            color = (lrgb != 0.) ? toLrgb(color2) : color2;        
           } else {
             gl_Position = vec4(position3,0,1);
-            color = color3;   
+            color = (lrgb != 0.) ? toLrgb(color3) : color3;   
           }
         }`,
   
@@ -111,27 +126,48 @@ function gradientMaker(options = {}) {
   function gradient4(regl) {
     return regl({
       frag: `
-        precision mediump float;
-        uniform vec4 color0;
-        uniform vec4 color1;
-        uniform vec4 color2;
-        uniform vec4 color3;
-        uniform vec2 iResolution;
-        uniform float lrgb;
-        float fn (float c) {
-          float tmp = abs(c);
-            if (tmp > 0.0031308) {
-                return 1.055 * pow(tmp, 1. / 2.4) - 0.055;
-          }
-            return c * 12.92;
+      precision highp float;
+      uniform vec4 color0;
+      uniform vec4 color1;
+      uniform vec4 color2;
+      uniform vec4 color3;
+      uniform vec2 iResolution;
+      uniform float lrgb;
+      float fnToRgb (float c) {
+        float tmp = abs(c);
+	      if (tmp > 0.0031308) {
+		      return 1.055 * pow(tmp, 1. / 2.4) - 0.055;
         }
-        void main () {
-          vec2 uv = gl_FragCoord.xy / iResolution.xy;
+	      return c * 12.92;
+      }
+      vec4 toRgb(vec4 color) {
+        return vec4 (fnToRgb(color.r), fnToRgb(color.g), fnToRgb(color.b), color.a);
+      }
+      float fnToLrgb (float c) {
+        float tmp = abs(c);
+        if (tmp <= 0.04045) {
+		      return c / 12.92;
+	      }
+	      return pow((tmp + 0.055) / 1.055, 2.4);
+      }
+      vec4 toLrgb(vec4 color) {
+        return vec4 (fnToLrgb(color.r), fnToLrgb(color.g), fnToLrgb(color.b), color.a);
+      }
+      void main () {
+        vec2 uv = gl_FragCoord.xy / iResolution.xy;
+        if (lrgb != 0.) {
+          vec4 colorLeft = mix(toLrgb(color2),toLrgb(color0),uv.y);
+          vec4 colorRight = mix(toLrgb(color3),toLrgb(color1),uv.y);
+          vec4 color = toRgb(mix(colorLeft,colorRight,uv.x));
+          gl_FragColor = color;
+        } else {
           vec4 colorLeft = mix(color2,color0,uv.y);
           vec4 colorRight = mix(color3,color1,uv.y);
           vec4 color = mix(colorLeft,colorRight,uv.x);
-          gl_FragColor = (lrgb != 0.) ? vec4(fn(color.r),fn(color.g),fn(color.b),color.a) : color;
-        }`,
+          gl_FragColor = color;
+        }
+
+      }`,
       vert: `
         attribute vec2 position;
         void main () {
